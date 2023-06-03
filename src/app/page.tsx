63ee1a39bcc8eecbeac51deab42f722e9ab9ce4b
ruthvik <ruthv1k@proton.me>
2023-06-03 17:04:00 +0530

    Update homepage to test if prisma is accessible globally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <main className='p-8'>
-      <h4 className='text-xl font-semibold'>Hello World</h4>
-    </main>
-  )
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+	const forms = await prisma.forms.findMany();
+
+	return (
+		<main className="p-8">
+			<h4 className="text-xl font-semibold">My Forms</h4>
+			{forms.length > 0 ? (
+				<ul>
+					{forms.map((form) => (
+						<li key={form.id}>{form.title}</li>
+					))}
+				</ul>
+			) : (
+				<span>No forms found</span>
+			)}
+		</main>
+	);
 }
