dee878c2c9f5b1131881faaf1d685690cf116c23
ruthvik <ruthv1k@proton.me>
2023-06-18 11:37:57 +0530

    Revalidate data inside dashboard page when a form is created

--- src/actions/forms.ts
+++ src/actions/forms.ts
@@ -1,11 +1,13 @@
 "use server";
 
 import { createForm, getForms } from "@/services/forms";
+import { revalidatePath } from "next/cache";
 
 export const getFormsAction = async (userId: string) => {
 	return await getForms(userId);
 };
 
 export const createFormAction = async (userId: string) => {
-	return await createForm(userId);
+	await createForm(userId);
+	revalidatePath("/dashboard");
 };
